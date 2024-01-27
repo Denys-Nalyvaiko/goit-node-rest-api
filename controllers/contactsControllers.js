468b6133e96fae2ab3fd937d65730c12b98ef3d5
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2024-01-27 18:13:27 +0100

    configure get and delete controllers

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,10 +1,54 @@
-import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import * as contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  const contactsList = await contactsService.listContacts();
 
-export const getOneContact = (req, res) => {};
+  res.status(200).json({
+    message: "Success!",
+    contacts: contactsList,
+  });
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  const contactId = req.params.id;
+  const contact = await contactsService.getContactById(contactId);
+
+  try {
+    if (!contact) {
+      throw HttpError(404);
+    }
+
+    res.status(200).json({
+      message: "Success!",
+      contact,
+    });
+  } catch ({ message }) {
+    res.status(404).json({
+      message,
+    });
+  }
+};
+
+export const deleteContact = async (req, res) => {
+  const contactId = req.params.id;
+  const contact = await contactsService.removeContact(contactId);
+
+  try {
+    if (!contact) {
+      throw HttpError(404);
+    }
+
+    res.status(200).json({
+      message: "Success!",
+      contact,
+    });
+  } catch ({ message }) {
+    res.status(404).json({
+      message,
+    });
+  }
+};
 
 export const createContact = (req, res) => {};
 
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,19 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string()
+    .required()
+    .message({ "any.required": "Missing required name field" }),
+  email: Joi.string()
+    .required()
+    .message({ "any.required": "Missing required email field" }),
+  phone: Joi.string()
+    .required()
+    .message({ "any.required": "Missing required phone field" }),
+});
 
 export const updateContactSchema = Joi.object({
-
-})
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+});
