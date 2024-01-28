d50e274b66c079810128e6ce0d2339d0531bbb72
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2024-01-28 14:59:45 +0100

    change try catch config

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -2,16 +2,23 @@ import HttpError from "../helpers/HttpError.js";
 import * as contactsService from "../services/contactsServices.js";
 
 export const getAllContacts = async (req, res) => {
-  const contactsList = await contactsService.listContacts();
+  try {
+    const contactsList = await contactsService.listContacts();
 
-  res.status(200).json(contactsList);
+    res.status(200).json(contactsList);
+  } catch ({ message }) {
+    res.status(404).json({
+      message,
+    });
+  }
 };
 
 export const getOneContact = async (req, res) => {
   const contactId = req.params.id;
-  const contact = await contactsService.getContactById(contactId);
 
   try {
+    const contact = await contactsService.getContactById(contactId);
+
     if (!contact) {
       throw HttpError(404);
     }
@@ -26,9 +33,10 @@ export const getOneContact = async (req, res) => {
 
 export const deleteContact = async (req, res) => {
   const contactId = req.params.id;
-  const contact = await contactsService.removeContact(contactId);
 
   try {
+    const contact = await contactsService.removeContact(contactId);
+
     if (!contact) {
       throw HttpError(404);
     }
@@ -43,9 +51,16 @@ export const deleteContact = async (req, res) => {
 
 export const createContact = async (req, res) => {
   const newContact = req.body;
-  const contact = await contactsService.addContact(newContact);
 
-  res.status(201).json(contact);
+  try {
+    const contact = await contactsService.addContact(newContact);
+
+    res.status(201).json(contact);
+  } catch ({ message }) {
+    res.status(404).json({
+      message,
+    });
+  }
 };
 
 export const updateContact = async (req, res) => {
@@ -60,9 +75,12 @@ export const updateContact = async (req, res) => {
     return;
   }
 
-  const contact = await contactsService.updateContact(contactId, targetContact);
-
   try {
+    const contact = await contactsService.updateContact(
+      contactId,
+      targetContact
+    );
+
     if (!contact) {
       throw HttpError(404);
     }
