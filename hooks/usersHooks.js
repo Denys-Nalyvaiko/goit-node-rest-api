c9b2cfbe632be25a0a428aaf656ea1df1906cd33
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2024-02-07 14:48:45 +0100

    protect salt rounds

--- hooks/usersHooks.js
+++ hooks/usersHooks.js
@@ -1,10 +1,15 @@
+import dotenv from "dotenv";
 import { hash } from "bcrypt";
+
+dotenv.config();
+
+const { SALT_ROUNDS } = process.env;
 
 export async function hashUsersPassword(next) {
   if (!this.isModified("password")) {
     return next();
   }
 
-  this.password = await hash(this.password, 10);
+  this.password = await hash(this.password, Number(SALT_ROUNDS) ?? 10);
   next();
 }
