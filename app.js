c61a2b37e383880fe36af290fdf0e2e51cf9ab3b
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2024-02-10 18:48:15 +0100

    configure public static

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static("public"));
+
 app.use("/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
 
